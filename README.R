de1a5d80bbf9dc6bae3a6e056f7a8688a1bb42ae
Nick Tustison <ntustison@gmail.com>
2015-05-07 18:31:21 -0700

    ENH:  Installation and initial testing.

--- README.R
+++ README.R
@@ -11,6 +11,9 @@ srcdir<-"./"
 buildrmd='ants2015build.Rmd'
 # rawrmds<-c("ants2015.Rmd","background/background1.Rmd","impact/impact1.Rmd","templates/templates.Rmd","evaluation/evaluation1.Rmd","evaluation/antsvfreesurfer2.Rmd","longitudinal/longitudinal.Rmd","lesions/lesions.Rmd","antsr/antsrdemo.Rmd","conclusion/conclusion.Rmd","lastslide.Rmd")
 rawrmds<-c("ants2015.Rmd","competitions/competitions.Rmd")
+
+rawrmds<-c("ants2015.Rmd","installation/ants_installation.Rmd","installation/antsr_installation.Rmd","installation/snap_installation.Rmd")
+
 # rawrmds<-c("ants2015.Rmd","evaluation/antsvfreesurfer.Rmd","longitudinal/longitudinal.Rmd","lesions/lesions.Rmd","antsr/antsrdemo.Rmd","conclusion/conclusion.Rmd","lastslide.Rmd")
 # rawrmds<-c("ants2015_GE.Rmd", "evaluation/antsvfreesurfer2.Rmd", "lesions/lesions.Rmd", "longitudinal/longitudinal.Rmd" )
 #rawrmds<-c( "ants2015_GE.Rmd", "philosophy/circularity/circularity.Rmd", "evaluation/antsvfreesurfer2.Rmd" )
