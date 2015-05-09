d2bd45ba4cb5307da729c5bd77a17ab491e0553f
Nick Tustison <ntustison@gmail.com>
2015-05-09 06:46:50 -0700

    ENH:  More stuff.

--- README.R
+++ README.R
@@ -9,14 +9,31 @@ library(rmarkdown)
 # library(revealjs)
 srcdir<-"./"
 buildrmd='ants2015build.Rmd'
+
+####################
+#
+#             May 19
+#
+####################
+
+########   Morning    ##########
+
+# Retrospective
+rawrmds <- c( "antsMni.Rmd", "background/background1.Rmd", "competitions/competitions.Rmd", "philosophy/circularity/circularity.Rmd" )
+
+# Research talk
+# rawrmds <- c( "evaluation/antsvfreesurfer2.Rmd", "evaluation/antsvfreesurfer2_extra.Rmd", "lesions/lesions.Rmd", "longitudinal/longitudinal2.Rmd" )
+
+########   Afternoon    ##########
+# rawrmds <- c( "ants2015.Rmd", "installation/ants_installation.Rmd", "installation/antsr_installation.Rmd", "installation/snap_installation.Rmd" )
+
+
+
+
 # rawrmds<-c("ants2015.Rmd","background/background1.Rmd","impact/impact1.Rmd","templates/templates.Rmd","evaluation/evaluation1.Rmd","evaluation/antsvfreesurfer2.Rmd","longitudinal/longitudinal.Rmd","lesions/lesions.Rmd","antsr/antsrdemo.Rmd","conclusion/conclusion.Rmd","lastslide.Rmd")
-rawrmds<-c("ants2015.Rmd","competitions/competitions.Rmd")
-
-rawrmds<-c("ants2015.Rmd","installation/ants_installation.Rmd","installation/antsr_installation.Rmd","installation/snap_installation.Rmd")
-
 # rawrmds<-c("ants2015.Rmd","evaluation/antsvfreesurfer.Rmd","longitudinal/longitudinal.Rmd","lesions/lesions.Rmd","antsr/antsrdemo.Rmd","conclusion/conclusion.Rmd","lastslide.Rmd")
 # rawrmds<-c("ants2015_GE.Rmd", "evaluation/antsvfreesurfer2.Rmd", "lesions/lesions.Rmd", "longitudinal/longitudinal.Rmd" )
-#rawrmds<-c( "ants2015_GE.Rmd", "philosophy/circularity/circularity.Rmd", "evaluation/antsvfreesurfer2.Rmd" )
+# rawrmds<-c( "ants2015_GE.Rmd", "philosophy/circularity/circularity.Rmd", "evaluation/antsvfreesurfer2.Rmd" )
 for ( x in 1:length(rawrmds) ) {
   if ( x == 1 )  cmd<-paste( "cat ",rawrmds[x]," > ",buildrmd ) else cmd<-paste( "cat ",rawrmds[x]," >> ",buildrmd )
   system(cmd)
