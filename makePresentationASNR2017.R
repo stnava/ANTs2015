fc1a782c80544693bdf8a177049425f422a98d7c
Nick Tustison <ntustison@gmail.com>
2017-04-23 18:42:48 -0700

    ENH:  ASNR 2017

---
+++ makePresentationASNR2017.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+
+####################
+#
+#   July 19, 2016
+#
+#
+#
+#
+#####################
+
+library( ggplot2 )
+library( rmarkdown )
+
+srcDir <- "./"
+buildrmd <- 'asnr2017.Rmd'
+
+########   Morning    ##########
+
+# Retrospective
+
+rawrmds <- c( "format.Rmd",
+              "background/background3.Rmd",
+              "competitions/competitions.Rmd",
+              "evaluation/antsvfreesurfer2.Rmd",
+              "antsr/antsr.Rmd",
+              "finalSlide.Rmd"
+               )
+
+for ( x in 1:length(rawrmds) ) {
+  if ( x == 1 )
+    {
+    cmd<-paste( "cat ", rawrmds[x], " > ", buildrmd )
+    } else {
+      cmd<-paste( "cat ", rawrmds[x], " >> ", buildrmd )
+    }
+  system( cmd )
+  }
+
+render( buildrmd, clean = TRUE )
